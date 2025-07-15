2189136aed86381f37e60a3eb5c67ad6eac0ca00
Somanath patil <patilsomanath292@gmail.com>
2025-07-15 22:22:29 +0530

    gsap

--- gsap/script.js
+++ gsap/script.js
@@ -1,5 +1,20 @@
-gsap.to('#box',{
-    x:1200,
-    duration:2,
-    delay:1 
-})
+// gsap.from('#box',{
+//     x:1200,
+//     duration:2,
+//     delay:1, 
+//     backgroundColor: "red",
+//     rotate:360, 
+//     borderRadius:"50%",
+//     scale:0.5,
+
+// })
+
+
+
+// gsap.from("h1",{
+//     y:60,
+//     duration:1,
+//     opacity:0,
+//     delay:1,
+//     stagger:0.2
+// })   
