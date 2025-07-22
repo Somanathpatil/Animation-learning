8c9d036ca5db91ae388bb7d19f72fe2199afdc52
Somanathpatil <patilsomanath292@gmail.com>
2025-07-22 19:54:56 +0530

    lenies end

--- lenis/script.js
+++ lenis/script.js
@@ -1,9 +1,30 @@
-// Initialize Lenis
-const lenis = new Lenis({
-  autoRaf: true,
-});
+// const lenis = new lenis()
 
-// Listen for the scroll event and log the event data
-lenis.on('scroll', (e) => {
-  console.log(e);
-});
+// function raf(time){
+//     lenis.raf(time)
+//     requestAnimationFrame(raf)
+// } 
+
+// requestAnimationFrame(raf)
+
+
+// // Initialize Lenis
+// const lenis = new Lenis({
+//   autoRaf: true,
+// });
+
+// // Listen for the scroll event and log the event data
+// lenis.on('scroll', (e) => {
+//   console.log(e);
+// });
+
+// Initialize Lenis
+const lenis = new Lenis();
+
+// Use requestAnimationFrame to continuously update the scroll
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
